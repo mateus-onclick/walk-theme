d16f52eddb1a941a8fc35b8828a97975d705e2b9
Fernando Moreira <fernando@fernandomoreiraweb.com>
2013-07-30 22:50:27 -0300

    banner ads, walk-logo, search nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 (function($){
 
-
+    $(".dropdown").on({
+        mouseenter: function(){
+            $(this).addClass("open");
+        },
+        mouseleave: function(){
+            $(this).removeClass("open");
+        }
+    });
 
 })(jQuery);
