9ea8e8388ee9122d6ec170bcf3fd1ae887a23320
Fernando Moreira <fernando@fernandomoreiraweb.com>
2013-12-23 23:21:23 -0200

    easing, Modernizr... go to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,33 @@
         }
     });
 
+    $('.tips').tooltip();
+
+    // GoToTop
+    $('#gototop').hide();
+
+    $("#gototop a[href^='#']").click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1000, 'easeInOutExpo' );
+        return false;
+    });
+
+    $('#gototop').on({
+        mouseenter: function(){
+            $(this).stop().animate({'bottom':'15px'}, 500, 'easeOutElastic' );
+        },
+        mouseleave: function(){
+            $(this).stop().animate({'bottom':'5px'}, 500, 'easeOutElastic' );
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#gototop').fadeIn(600);
+        }
+        else {
+            $('#gototop').fadeOut(600);
+        }
+    });
+    // GoToTop
+
 })(jQuery);
